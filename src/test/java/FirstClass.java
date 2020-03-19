ef7b553dc6958c516991b67eec7db8c48d1b9518
violettager <58573873+violettager@users.noreply.github.com>
2020-03-18 21:18:11 -0400

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,5 +2,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("change one");
+        System.out.println("change two");
+        System.out.println("change three");
     }
 }
